a902fe24f8c723b7124ebf498ae82ab6d25a1261
kiheyunkim <kiheyunkim@gmail.com>
2020-04-16 17:32:20 +0900

    commit

--- SpringCorePractice/SpringTest5/src/main/java/com/kiheyunkim/kim/reservation/controller/ReservationController.java
+++ SpringCorePractice/SpringTest5/src/main/java/com/kiheyunkim/kim/reservation/controller/ReservationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
